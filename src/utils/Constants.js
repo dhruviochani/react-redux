5711dc94ad0dfd22060c920ccb2b82625f0a3e8d
Dhruvi Ochani <94130683+dhruviochani@users.noreply.github.com>
2022-09-13 13:04:04 +0530

    needy updated

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,4 @@
 export const LOGIN_URL = 'https://jsonplaceholder.typicode.com/users';
 export const DASH_URL='https://jsonplaceholder.typicode.com/users';
+// export const LOGIN_URL = 'http://182.70.249.68:9001/login';
+// export const DASH_URL='http://182.70.249.68:9001/dashboard';
